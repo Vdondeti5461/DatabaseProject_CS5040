70661f4687ce3faadd3d93464a5b71bd54f334e1
Vamsi Naidu Dondeti <144267965+Vdondeti5461@users.noreply.github.com>
2023-12-08 16:31:18 -0500

    Create Insertdata.sql
    Inserting data into created tables

---
+++ Insertdata.sql
@@ -0,0 +1,19 @@
+--users 1
+
+INSERT ALL
+  INTO Users (UserID, Username, Password, Email, CreatedAt, UpdatedAt) VALUES (1, 'user1', 'pass1', 'user1@example.com', SYSDATE, SYSDATE)
+  INTO Users (UserID, Username, Password, Email, CreatedAt, UpdatedAt) VALUES (2, 'user2', 'pass2', 'user2@example.com', SYSDATE, SYSDATE)
+  INTO Users (UserID, Username, Password, Email, CreatedAt, UpdatedAt) VALUES (3, 'user3', 'pass3', 'user3@example.com', SYSDATE, SYSDATE)
+  INTO Users (UserID, Username, Password, Email, CreatedAt, UpdatedAt) VALUES (4, 'user4', 'pass4', 'user4@example.com', SYSDATE, SYSDATE)
+  INTO Users (UserID, Username, Password, Email, CreatedAt, UpdatedAt) VALUES (5, 'user5', 'pass5', 'user5@example.com', SYSDATE, SYSDATE)
+  INTO Users (UserID, Username, Password, Email, CreatedAt, UpdatedAt) VALUES (6, 'user6', 'pass6', 'user6@example.com', SYSDATE, SYSDATE)
+  INTO Users (UserID, Username, Password, Email, CreatedAt, UpdatedAt) VALUES (7, 'user7', 'pass7', 'user7@example.com', SYSDATE, SYSDATE)
+  INTO Users (UserID, Username, Password, Email, CreatedAt, UpdatedAt) VALUES (8, 'user8', 'pass8', 'user8@example.com', SYSDATE, SYSDATE)
+  INTO Users (UserID, Username, Password, Email, CreatedAt, UpdatedAt) VALUES (9, 'user9', 'pass9', 'user9@example.com', SYSDATE, SYSDATE)
+  INTO Users (UserID, Username, Password, Email, CreatedAt, UpdatedAt) VALUES (10, 'user10', 'pass10', 'user10@example.com', SYSDATE, SYSDATE)
+  INTO Users (UserID, Username, Password, Email, CreatedAt, UpdatedAt) VALUES (11, 'user11', 'pass11', 'user11@example.com', SYSDATE, SYSDATE)
+  INTO Users (UserID, Username, Password, Email, CreatedAt, UpdatedAt) VALUES (12, 'user12', 'pass12', 'user12@example.com', SYSDATE, SYSDATE)
+  INTO Users (UserID, Username, Password, Email, CreatedAt, UpdatedAt) VALUES (13, 'user13', 'pass13', 'user13@example.com', SYSDATE, SYSDATE)
+  INTO Users (UserID, Username, Password, Email, CreatedAt, UpdatedAt) VALUES (14, 'user14', 'pass14', 'user14@example.com', SYSDATE, SYSDATE)
+  INTO Users (UserID, Username, Password, Email, CreatedAt, UpdatedAt) VALUES (15, 'user15', 'pass15', 'user15@example.com', SYSDATE, SYSDATE)
+SELECT * FROM dual;
