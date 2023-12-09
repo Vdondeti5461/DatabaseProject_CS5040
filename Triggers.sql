e3b5f84955c594f9a64cad808bdfb887d97077fa
Vamsi Naidu Dondeti <144267965+Vdondeti5461@users.noreply.github.com>
2023-12-09 15:23:37 -0500

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -15,3 +15,33 @@ BEGIN
   VALUES (LogID_SEQ.NEXTVAL, :NEW.UserID, 'User logged in', SYSDATE);
 END;
 /
+
+
+--Second Trigger 
+
+-- creating a sequece CREATE SEQUENCE DepositID_SEQ START WITH 16 INCREMENT BY 1 NOMAXVALUE;
+
+
+CREATE OR REPLACE TRIGGER UpdateAccountAfterDeposit
+AFTER INSERT ON Deposits
+FOR EACH ROW
+DECLARE
+  v_Balance Accounts.Balance%TYPE;
+BEGIN
+  -- Retrieve the current balance of the user
+  SELECT Balance INTO v_Balance
+  FROM Accounts
+  WHERE UserID = (SELECT UserID FROM Wallets WHERE WalletID = :NEW.WalletID);
+
+  -- Update the balance
+  UPDATE Accounts
+  SET Balance = v_Balance + :NEW.Amount
+  WHERE UserID = (SELECT UserID FROM Wallets WHERE WalletID = :NEW.WalletID);
+EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    RAISE_APPLICATION_ERROR(-20001, 'Account not found for deposit.');
+END;
+/
+
+-- then test the use case 
+
