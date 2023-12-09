84d5afbc5bd73e6ef4e072ac6326e57f65dfc20b
Vamsi Naidu Dondeti <144267965+Vdondeti5461@users.noreply.github.com>
2023-12-08 19:13:04 -0500

    Create select.sql
    Select * info for all tables

---
+++ select.sql
@@ -0,0 +1,83 @@
+-- Users Table
+SELECT * FROM Users;
+
+-- UserProfiles Table
+SELECT * FROM UserProfiles;
+
+-- Accounts Table
+SELECT * FROM Accounts;
+
+-- Instruments Table
+SELECT * FROM Instruments;
+
+-- Exchanges Table
+SELECT * FROM Exchanges;
+
+-- InstrumentExchangeMapping Table
+SELECT * FROM InstrumentExchangeMapping;
+
+-- Orders Table
+SELECT * FROM Orders;
+
+-- OrderDetails Table
+SELECT * FROM OrderDetails;
+
+-- Transactions Table
+SELECT * FROM Transactions;
+
+-- TransactionHistory Table
+SELECT * FROM TransactionHistory;
+
+-- Wallets Table
+SELECT * FROM Wallets;
+
+-- Deposits Table
+SELECT * FROM Deposits;
+
+-- Withdrawals Table
+SELECT * FROM Withdrawals;
+
+-- APIKeys Table
+SELECT * FROM APIKeys;
+
+-- UserSessions Table
+SELECT * FROM UserSessions;
+
+-- Watchlists Table
+SELECT * FROM Watchlists;
+
+-- WatchlistItems Table
+SELECT * FROM WatchlistItems;
+
+-- Notifications Table
+SELECT * FROM Notifications;
+
+-- Roles Table
+SELECT * FROM Roles;
+
+-- UserRoles Table
+SELECT * FROM UserRoles;
+
+-- Logs Table
+SELECT * FROM Logs;
+
+-- Settings Table
+SELECT * FROM Settings;
+
+-- UserGroups Table
+SELECT * FROM UserGroups;
+
+-- UserGroupMembers Table
+SELECT * FROM UserGroupMembers;
+
+-- Portfolios Table
+SELECT * FROM Portfolios;
+
+-- PortfolioInstruments Table
+SELECT * FROM PortfolioInstruments;
+
+-- AuditTrail Table
+SELECT * FROM AuditTrail;
+
+-- MarketData Table
+SELECT * FROM MarketData;
