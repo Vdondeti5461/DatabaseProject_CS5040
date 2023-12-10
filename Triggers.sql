7660e5c1510b128067be08115b19f2d117dcb17a
Vamsi Naidu Dondeti <144267965+Vdondeti5461@users.noreply.github.com>
2023-12-09 22:42:27 -0500

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -3,7 +3,7 @@
 --First trigger
 -- Ensure to create sequences before creating trigger . Need two sequences to test this trigger
 --1 CREATE SEQUENCE SessionID_SEQ START WITH 16 INCREMENT BY 1 NOMAXVALUE;
---2 CREATE SEQUENCE LogID_SEQ START WITH 1 INCREMENT BY 1 NOMAXVALUE;
+--2 CREATE SEQUENCE LogID_SEQ START WITH 16 INCREMENT BY 1 NOMAXVALUE;
 
 --Trigger LogUserLogin
 
