5346ad9b76510115d748ae7ea7a3e7de055a6fc2
Vamsi Naidu Dondeti <144267965+Vdondeti5461@users.noreply.github.com>
2023-12-10 13:39:55 -0500

    Update Functions.sql

--- Functions.sql
+++ Functions.sql
@@ -125,4 +125,27 @@ END;
 /
 
 
+--Function 7
+
+CREATE OR REPLACE FUNCTION GetUserRecentTransaction(p_UserID IN Users.UserID%TYPE)
+RETURN VARCHAR2 IS
+  v_TransactionDetails VARCHAR2(4000);
+BEGIN
+  SELECT 'TransactionID: ' || TransactionID || ', OrderID: ' || OrderID || ', Timestamp: ' || Timestamp
+  INTO v_TransactionDetails
+  FROM Transactions
+  WHERE OrderID IN (SELECT OrderID FROM Orders WHERE UserID = p_UserID)
+  ORDER BY Timestamp DESC
+  FETCH FIRST 1 ROW ONLY;
+
+  RETURN v_TransactionDetails;
+EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    RETURN 'No transactions found for the user.';
+  WHEN OTHERS THEN
+    RAISE_APPLICATION_ERROR(-20026, 'Error occurred: ' || SQLERRM);
+END;
+/
+
+
 
