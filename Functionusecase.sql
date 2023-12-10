e126d8583e750781ec8cb2a38fcc50db2f73ef56
Vamsi Naidu Dondeti <144267965+Vdondeti5461@users.noreply.github.com>
2023-12-09 22:11:49 -0500

    Update Functionusecase.sql

--- Functionusecase.sql
+++ Functionusecase.sql
@@ -7,3 +7,33 @@
 SELECT GetUserEmail(1) FROM dual;
 SELECT GetUserEmail(999) FROM dual; -- Assuming 999 does not exist
 
+
+--second function
+
+--Testing to check userbalance / Not available
+
+SELECT GetUserBalance(1) FROM dual;
+SELECT GetUserBalance(999) FROM dual; 
+
+
+--Third Function
+
+--Testing to count orders
+
+SELECT CountUserOrders(1) FROM dual;
+SELECT CountUserOrders(999) FROM dual;
+
+--Fourth Function
+
+SELECT GetInstrumentType(1) FROM dual; 
+SELECT GetInstrumentType(999) FROM dual; 
+
+--Fifth Function
+
+SELECT GetTotalDeposits(1) FROM dual; 
+SELECT GetTotalDeposits(999) FROM dual;
+
+
+
+
+
