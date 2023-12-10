e079f9333bbc02aff59e0cc48e20548172f3f801
Vamsi Naidu Dondeti <144267965+Vdondeti5461@users.noreply.github.com>
2023-12-09 20:12:29 -0500

    Update indexesusecase.sql

--- indexesusecase.sql
+++ indexesusecase.sql
@@ -2,7 +2,11 @@
 
 SELECT * FROM Transactions WHERE Timestamp BETWEEN TO_DATE('2023-01-01', 'YYYY-MM-DD') AND TO_DATE('2023-01-31', 'YYYY-MM-DD');
 
---second index usecase
+--second index Usecase
 
 SELECT * FROM Users WHERE Username = 'specificUsername';
+
+--Third index Usecase
+SELECT * FROM Users WHERE Email = 'vampe@example.com';
+
 
