714a85ce948ba38e5a7a8fc2216a7e3d80463f58
Vamsi Naidu Dondeti <144267965+Vdondeti5461@users.noreply.github.com>
2023-12-09 23:02:51 -0500

    Update indexesusecase.sql

--- indexesusecase.sql
+++ indexesusecase.sql
@@ -2,6 +2,11 @@
 
 SELECT * FROM Transactions WHERE Timestamp BETWEEN TO_DATE('2023-01-01', 'YYYY-MM-DD') AND TO_DATE('2023-01-31', 'YYYY-MM-DD');
 
+--changing time period to get data and above query give no data
+
+SELECT * FROM Transactions WHERE Timestamp BETWEEN TO_DATE('2023-12-01', 'YYYY-MM-DD') AND TO_DATE('2023-12-31', 'YYYY-MM-DD');
+
+
 --second index Usecase
 
 SELECT * FROM Users WHERE Username = 'specificUsername';
