8cfdcba2c5975b9377c876420d55da05069694ce
Vamsi Naidu Dondeti <144267965+Vdondeti5461@users.noreply.github.com>
2023-12-09 19:39:32 -0500

    Create packageusecase.sql

---
+++ packageusecase.sql
@@ -0,0 +1,37 @@
+--create sequence CREATE SEQUENCE UserID_SEQ START WITH 16 INCREMENT BY 1 NOMAXVALUE; create sequence if not available
+
+--first test case AddNewUser
+
+BEGIN
+    UserManagement.AddNewUser('uniqueUsernameTest', 'passwordTest', 'uniqueEmailTest@example.com');
+END;
+/
+
+
+--second Updateuserprofile
+  
+BEGIN
+    UserManagement.UpdateUserProfile(1, 'FirstName', 'LastName', TO_DATE('1990-01-01', 'YYYY-MM-DD'), 'Country');
+END;
+/
+
+--third changeuserpassword
+
+BEGIN
+    UserManagement.ChangeUserPassword(1, 'newPassword');
+END;
+/
+
+--Fourth GetUseremail
+
+DECLARE
+    v_Email VARCHAR2(255);
+BEGIN
+    v_Email := UserManagement.GetUserEmail(1); -- Use a valid UserID
+    DBMS_OUTPUT.PUT_LINE('Email: ' || v_Email);
+END;
+/
+
+
+
+
