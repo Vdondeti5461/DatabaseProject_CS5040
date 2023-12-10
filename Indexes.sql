0bf94ed42d8c0246a658e3a732018bc23467f2eb
Vamsi Naidu Dondeti <144267965+Vdondeti5461@users.noreply.github.com>
2023-12-09 20:11:26 -0500

    Update Indexes.sql

--- Indexes.sql
+++ Indexes.sql
@@ -6,3 +6,7 @@ CREATE INDEX idx_transactions_timestamp ON Transactions (Timestamp);
 
 CREATE INDEX idx_users_username ON Users (Username);
 
+--Third Index
+
+CREATE INDEX idx_users_email ON Users (Email);
+
