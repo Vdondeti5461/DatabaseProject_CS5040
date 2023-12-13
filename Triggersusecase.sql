a4d8a1a8213b30ab667c2541820f64e51d5c4353
Vamsi Naidu Dondeti <144267965+Vdondeti5461@users.noreply.github.com>
2023-12-13 12:18:19 -0500

    Update Triggersusecase.sql

--- Triggersusecase.sql
+++ Triggersusecase.sql
@@ -2,7 +2,7 @@
 
 -- Insert a mock session
 INSERT INTO UserSessions (SessionID, UserID, Token, ExpiresAt)
-VALUES (SessionID_SEQ.NEXTVAL, 1, 'token123', SYSDATE + 7);
+VALUES (SessionID_SEQ.NEXTVAL, 1, 'NikVam', SYSDATE + 7);
 
 -- Query the Logs table to verify the trigger action
 SELECT * FROM Logs WHERE UserID = 1;
@@ -12,7 +12,7 @@ SELECT * FROM Logs WHERE UserID = 1;
 
 -- Insert a mock deposit (replace the WalletID and UserID with actual values from your database)
 INSERT INTO Deposits (DepositID, WalletID, Amount, Timestamp)
-VALUES (DepositID_SEQ.NEXTVAL, [Your WalletID], 1000.00, SYSDATE);
+VALUES (DepositID_SEQ.NEXTVAL, 1, 1000.00, SYSDATE);
 
 -- Query the Accounts table to verify the trigger action (replace the UserID with the actual UserID linked to the WalletID used above)
 SELECT * FROM Accounts WHERE UserID = [Your UserID];
@@ -22,7 +22,7 @@ SELECT * FROM Accounts WHERE UserID = [Your UserID];
 
 -- Attempt a mock withdrawal (replace the WalletID with an actual value from your database and set an Amount greater than the account balance)
 INSERT INTO Withdrawals (WithdrawalID, WalletID, Amount, Timestamp)
-VALUES (WithdrawalID_SEQ.NEXTVAL, [Your WalletID], [Amount Greater Than Balance], SYSDATE);
+VALUES (WithdrawalID_SEQ.NEXTVAL, 1, 11000, SYSDATE);
 
 --if withdrawl amount greated than balance it will throw error as insufficient funds
 
@@ -30,8 +30,8 @@ VALUES (WithdrawalID_SEQ.NEXTVAL, [Your WalletID], [Amount Greater Than Balance]
 
 -- Update a user's password (replace UserID and Password with appropriate values)
 UPDATE Users
-SET Password = 'newPassword'
-WHERE UserID = [Your UserID];
+SET Password = 'NikCHVam'
+WHERE UserID = 1;
 
 --after update check user table for password change for userid
 
@@ -39,8 +39,8 @@ WHERE UserID = [Your UserID];
 
 -- Update a user's profile (replace ProfileID with an actual value)
 UPDATE UserProfiles
-SET FirstName = 'UpdatedFirstName', LastName = 'UpdatedLastName'
-WHERE ProfileID = [Your ProfileID];
+SET FirstName = 'RajaLe', LastName = 'Manra'
+WHERE ProfileID = 1;
 
 -- Query the Logs table to verify the trigger action
 SELECT * FROM Logs WHERE UserID = [Your UserID] AND Action = 'User profile updated';
@@ -49,15 +49,15 @@ SELECT * FROM Logs WHERE UserID = [Your UserID] AND Action = 'User profile updat
 
 -- Update an order's status (replace OrderID with an actual value and set a new status)
 UPDATE Orders
-SET Status = 'NewStatus'
-WHERE OrderID = [Your OrderID];
+SET Status = 'Executed'
+WHERE OrderID = 1;
 
 -- Query the Notifications table to verify the trigger action
-SELECT * FROM Notifications WHERE UserID = [Associated UserID with Order];
+SELECT * FROM Notifications WHERE UserID = 1;
 
 
 -- Query the AuditTrail table to verify the trigger action
-SELECT * FROM AuditTrail WHERE UserID = [Your UserID];
+SELECT * FROM AuditTrail WHERE UserID = 1;
 
 --Seventh Trigger Use case
 
@@ -65,7 +65,7 @@ SELECT * FROM AuditTrail WHERE UserID = [Your UserID];
 
 UPDATE Accounts
 SET Balance = Balance + 100 
-WHERE AccountID = [Some AccountID];
+WHERE AccountID = 1;
 
 --check logs table
 
