643b9549f244dd82e5ab2e50e4891de486ecb692
Nikhil Choppa <69682190+nikhilchoppa@users.noreply.github.com>
2023-12-13 13:24:33 -0500

    Update Functionusecase.sql

--- Functionusecase.sql
+++ Functionusecase.sql
@@ -5,7 +5,10 @@
 --Testing with valid usedid / Invalid UserId
 
 SELECT GetUserEmail(1) FROM dual;
-SELECT GetUserEmail(999) FROM dual; -- Assuming 999 does not exist
+
+-- Assuming 999 does not exist
+
+SELECT GetUserEmail(999) FROM dual; 
 
 
 --second function
@@ -38,45 +41,44 @@ SELECT GetTotalDeposits(999) FROM dual;
 SELECT CalculateTotalPortfolioValue_V3(1) FROM dual; 
 SELECT CalculateTotalPortfolioValue_V3(999) FROM dual; 
 
---seventh Funtion
+-- Seventh Function
 
 SELECT GetUserRecentTransaction(1) FROM dual; 
 SELECT GetUserRecentTransaction(999) FROM dual; 
 
---testing another way
+-- testing another way
 
-INSERT INTO Users (UserID, Username, Password, Email, CreatedAt, UpdatedAt) 
-VALUES (1, 'TestUser', 'TestPass', 'testuser@example.com', SYSDATE, SYSDATE);
+INSERT INTO Users (UserID, Username, Password, Email, CreatedAt, UpdatedAt) VALUES (1, 'TestUser', 'TestPass', 'testuser@example.com', SYSDATE, SYSDATE);
 
---Inserting multiple orders to user1
+-- Inserting multiple orders to user1
 
 -- Inserting first order
-INSERT INTO Orders (OrderID, UserID, InstrumentID, Type, Status, CreatedAt, UpdatedAt) 
-VALUES (101, 1, 1, 'Buy', 'Open', SYSDATE - 7, SYSDATE - 7);
+
+INSERT INTO Orders (OrderID, UserID, InstrumentID, Type, Status, CreatedAt, UpdatedAt) VALUES (111, 1, 1, 'Buy', 'Open', SYSDATE - 7, SYSDATE - 7);
 
 -- Inserting second order
-INSERT INTO Orders (OrderID, UserID, InstrumentID, Type, Status, CreatedAt, UpdatedAt) 
-VALUES (102, 1, 2, 'Sell', 'Closed', SYSDATE - 5, SYSDATE - 4);
+
+INSERT INTO Orders (OrderID, UserID, InstrumentID, Type, Status, CreatedAt, UpdatedAt) VALUES (102, 1, 2, 'Sell', 'Closed', SYSDATE - 5, SYSDATE - 4);
 
 -- Inserting third order (most recent)
-INSERT INTO Orders (OrderID, UserID, InstrumentID, Type, Status, CreatedAt, UpdatedAt) 
-VALUES (103, 1, 3, 'Buy', 'Executed', SYSDATE - 1, SYSDATE);
 
---Inserting transactions to orders
+INSERT INTO Orders (OrderID, UserID, InstrumentID, Type, Status, CreatedAt, UpdatedAt) VALUES (103, 1, 3, 'Buy', 'Executed', SYSDATE - 1, SYSDATE);
+
+-- Inserting transactions to orders
 
 -- Inserting a transaction for the first order
-INSERT INTO Transactions (TransactionID, OrderID, Timestamp) 
-VALUES (201, 101, SYSDATE - 7); -- Assuming TransactionID starts at 201
+
+INSERT INTO Transactions (TransactionID, OrderID, Timestamp) VALUES (201, 101, SYSDATE - 7); -- Assuming TransactionID starts at 201
 
 -- Inserting a transaction for the second order
-INSERT INTO Transactions (TransactionID, OrderID, Timestamp) 
-VALUES (202, 102, SYSDATE - 5);
 
--- Inserting a transaction for the third (most recent) order
-INSERT INTO Transactions (TransactionID, OrderID, Timestamp) 
-VALUES (203, 103, SYSDATE - 1);
+INSERT INTO Transactions (TransactionID, OrderID, Timestamp) VALUES (202, 102, SYSDATE - 5);
 
---Getting recent transactions 
+-- Inserting a transaction for the third (most recent) order]
+
+INSERT INTO Transactions (TransactionID, OrderID, Timestamp) VALUES (203, 103, SYSDATE - 1);
+
+-- Getting recent transactions 
 
 SELECT t.TransactionID, t.OrderID, t.Timestamp, o.UserID, o.Type, o.Status
 FROM Transactions t
